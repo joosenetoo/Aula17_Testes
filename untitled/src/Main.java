209d7390502fe284008d0f5ad0615813305f3d0e
joosenetoo <jose_neto_mendes@hotmail.com>
2024-08-19 15:46:25 -0300

    first

---
+++ untitled/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+//        // Criando um objeto da classe Carro
+//        Carro meuCarro = new Carro("Volkswagen", "Fusca", 1971);
+//
+//        // Testando os métodos da classe
+//        System.out.println(meuCarro.ligar());
+//        System.out.println(meuCarro.status());
+//        System.out.println(meuCarro.desligar());
+//        System.out.println(meuCarro.status());
+
+
+        }
+    }
---
+++ untitled/src/MainTeste.java
@@ -0,0 +1,19 @@
+public class MainTeste {
+
+    public static void main(String[] args) {
+
+    String nome_aluno1 = "João";
+    String nome_aluno2 = "Maria";
+    String nome_aluno3 = "Pedro";
+    String nome_aluno4 = "Lara";
+
+    System.out.println("Nome: " + nome_aluno1);
+    System.out.println("Nome: " + nome_aluno2);
+    System.out.println("Nome: " + nome_aluno3);
+    System.out.println("Nome: " + nome_aluno4);
+
+
+    }
+
+
+}
